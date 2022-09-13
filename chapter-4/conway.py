1330f5c40f7e756560da9baafb8900f2a78799d9
Scott Gordon <5c077.60rd0n@gmail.com>
2022-09-12 23:30:21 -0400

    fix logic errors

--- chapter-4/conway.py
+++ chapter-4/conway.py
@@ -1,6 +1,5 @@
 # Conway's Game of Life
 import random, time, copy
-from turtle import left
 WIDTH = 60
 HEIGHT = 20
 
@@ -10,6 +9,8 @@ for x in range(WIDTH):
     column = [] # Create a new column.
     for y in range(HEIGHT):
         if random.randint(0, 1) == 0:
+            column.append('#') # Add a living cell.
+        else:
             column.append(' ') # Add a dead cell.
     next_cells.append(column) # next_cells is a list of column lists.
     
