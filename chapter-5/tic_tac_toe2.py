a4140be4315035836819e9118615707a65ab53c4
Scott Gordon <5c077.60rd0n@gmail.com>
2022-12-10 20:16:49 -0500

    Add chapter 5

---
+++ chapter-5/tic_tac_toe2.py
@@ -0,0 +1,10 @@
+from tic_tac_toe import print_board
+the_board = {
+    'top-L': 'O', 'top-M': 'O', 'top-R': 'O',
+    'mid-L': 'X', 'mid-M': 'X', 'mid-R': 'O',
+    'low-L': ' ', 'low-M': ' ', 'low-R': 'X',
+}
+
+print_board(the_board)
+
+print_board(the_board)
